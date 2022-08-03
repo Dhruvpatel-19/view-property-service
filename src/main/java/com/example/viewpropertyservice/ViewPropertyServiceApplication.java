083fe7b446677d7e9bfc8946c08ab17e5fadb739
Dhruv <dhruv.p@simformsolutions.com>
2022-08-03 14:20:58 +0530

    Created entities

---
+++ src/main/java/com/example/viewpropertyservice/ViewPropertyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.viewpropertyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewPropertyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewPropertyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/viewpropertyservice/ViewPropertyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viewpropertyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewPropertyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
