1c6c19c6cbd7ef974840b97aec5083d830c2e145
Jay Rathod <jay.rathod@simformsolutions.com>
2022-08-26 15:51:58 +0530

    added put method to add favourites again

--- src/main/java/com/example/viewpropertyservice/controller/AddToFavouritesController.java
+++ src/main/java/com/example/viewpropertyservice/controller/AddToFavouritesController.java
@@ -4,10 +4,12 @@ import com.example.viewpropertyservice.entity.Favourites;
 import com.example.viewpropertyservice.entity.Property;
 import com.example.viewpropertyservice.entity.User;
 import com.example.viewpropertyservice.jwt.JwtUtil;
+import com.example.viewpropertyservice.repository.PropertyRepository;
 import com.example.viewpropertyservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +28,8 @@ public class AddToFavouritesController { //here we assume that we have JWT token
 @Autowired JwtUtil jwtUtil;
 @Autowired UserRepository userRepository;
 
+@Autowired PropertyRepository propertyRepository;
+
 
 /*
 
@@ -39,34 +43,24 @@ public class AddToFavouritesController { //here we assume that we have JWT token
 
 
 
-  @PutMapping(value="/addToFavourites")
-  public ResponseEntity<User> addToFavourites(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, @RequestBody Property property) throws Exception {
+  @PutMapping(value="/addToFavourites/{propertyId}")
+  public ResponseEntity<User> addToFavourites(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, @PathVariable("propertyId") int propertyId) throws Exception {
 
     String requestTokenHeader = httpServletRequest.getHeader("Authorization");
     String jwtToken = null;
     String email = null;
-
     if(requestTokenHeader!=null && requestTokenHeader.startsWith("Bearer ")) {
         email = jwtUtil.extractUsername(requestTokenHeader.substring(7));
     }
 
-
       User user = userRepository.findByEmail(email); //get the email from token
-//      property.setCreatedAt(LocalDateTime.now());
       Favourites favourites =new Favourites();
       favourites.setCreatedAt(LocalDateTime.now());
+      Property property=propertyRepository.findByPropertyId(propertyId);
       favourites.setProperty(property);
       List<Favourites> favList= user.getFavourites(); //get the list of favourites entity.
       favList.add(favourites);// add the newly created favourite object to the favourites list
       user.setFavourites(favList);
-
-//      List<Property> userPropertyList = user.getPropertyList();
-//      userPropertyList.add(property);
-//      userRepository.save(user);
-
-
-
-
     return new ResponseEntity<User>(userRepository.save(user), HttpStatus.OK);
   }
 
